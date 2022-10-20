c3142b75572f2631e7ca3140d8032d0d020cd807
shukubota <kubotashu0421@gmail.com>
2022-10-20 13:49:23 +0900

    :hammer:

---
+++ .circleci/test.sh
@@ -0,0 +1,25 @@
+test1=$1
+test2=$2
+test3=$3
+test4=$4
+test5=$5
+test6=$6
+test7=$7
+test8=$8
+test9=$9
+test10=${10}
+test11=${11}
+test12=${12}
+
+echo test1 $test1
+echo test2 $test2
+echo test3 $test3
+echo test4 $test4
+echo test5 $test5
+echo test6 $test6
+echo test7 $test7
+echo test8 $test8
+echo test9 $test9
+echo test10 $test10
+echo test11 $test11
+echo test12 $test12
