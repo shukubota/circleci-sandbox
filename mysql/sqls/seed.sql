78eb008baf49eb2a5c84091462dc84be693b3ff5
shukubota <kubotashu0421@gmail.com>
2024-08-12 22:06:35 +0900

    gap lock起こす構造

--- mysql/sqls/seed.sql
+++ mysql/sqls/seed.sql
@@ -2,3 +2,7 @@ create table member_tokens (
     id varchar(255) primary key,
     deleted_at datetime
 );
+
+alter table member_tokens add column member_id varchar(255) not null;
+
+create index member_tokens_member_id_index on member_tokens (member_id);
