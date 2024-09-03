e150b18d2ca88d365ec1fa988f63a215764b95b7
shukubota <kubotashu0421@gmail.com>
2024-09-03 23:36:07 +0900

    試しに競合しそうなsql

--- mysql/sqls/seed.sql
+++ mysql/sqls/seed.sql
@@ -1,8 +1,43 @@
-create table member_tokens (
-    id varchar(255) primary key,
+create table tokens2 (
+    id varchar(255) not null,
     deleted_at datetime
 );
 
-alter table member_tokens add column member_id varchar(255) not null;
+###########################thread1###########################
 
-create index member_tokens_member_id_index on member_tokens (member_id);
+insert into tokens2(id)
+values
+    ('9cebe7ab-d43c-4a28-9caf-571a79ee7e3a'),
+    ('9cebe7ab-d43c-4a28-9caf-571a79ee7e3b'),
+    ('9cebe7ab-d43c-4a28-9caf-571a79ee7e3c'),
+    ('9cebe7ab-d43c-4a28-9caf-571a79ee7e3d'),
+    ('9cebe7ab-d43c-4a28-9caf-571a79ee7e3e'),
+    ('9cebe7ab-d43c-4a28-9caf-571a79ee7e3f');
+
+
+create index tokens2_id_idx
+    on tokens2 (id);
+
+start transaction;
+
+update tokens2 set deleted_at = now()
+where id = '9cebe7ab-d43c-4a28-9caf-571a79ee7e3a';
+
+insert into tokens2(id)
+values
+    ('9cebe7ab-d43c-4a28-9caf-571a79ee7e3g');
+
+rollback;
+###########################
+
+#################thread2#################
+start transaction;
+update tokens2 set deleted_at = now()
+where id = '9cebe7ab-d43c-4a28-9caf-571a79ee7e3d';
+
+insert into tokens2(id)
+values
+    ('9cebe7ab-d43c-4a28-9caf-571a79ee7e3h');
+
+rollback;
+#################
