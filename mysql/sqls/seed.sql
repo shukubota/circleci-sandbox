c722bce19b57f1fec536d516d56c6f1297aa75cc
shukubota <kubotashu0421@gmail.com>
2024-09-03 23:52:52 +0900

    クエリ修正

--- mysql/sqls/seed.sql
+++ mysql/sqls/seed.sql
@@ -1,41 +1,31 @@
-create table tokens2 (
-    id varchar(255) not null,
+create table members (
+    member_id varchar(255) not null,
     deleted_at datetime
 );
 
-###########################thread1###########################
+create index member_id_idx
+    on members (member_id);
 
-insert into tokens2(id)
-values
-    ('9cebe7ab-d43c-4a28-9caf-571a79ee7e3a'),
-    ('9cebe7ab-d43c-4a28-9caf-571a79ee7e3b'),
-    ('9cebe7ab-d43c-4a28-9caf-571a79ee7e3c'),
-    ('9cebe7ab-d43c-4a28-9caf-571a79ee7e3d'),
-    ('9cebe7ab-d43c-4a28-9caf-571a79ee7e3e'),
-    ('9cebe7ab-d43c-4a28-9caf-571a79ee7e3f');
-
-
-create index tokens2_id_idx
-    on tokens2 (id);
+alter table members drop index member_id_idx;
 
 start transaction;
 
-update tokens2 set deleted_at = now()
-where id = '9cebe7ab-d43c-4a28-9caf-571a79ee7e3a';
+update members set deleted_at = now()
+where member_id not in ('9cebe7ab-d43c-4a28-9caf-571a79ee7e3g');
 
-insert into tokens2(id)
+insert into members(member_id)
 values
     ('9cebe7ab-d43c-4a28-9caf-571a79ee7e3g');
 
 rollback;
 ###########################
 
-#################thread2#################
+#################
 start transaction;
-update tokens2 set deleted_at = now()
-where id = '9cebe7ab-d43c-4a28-9caf-571a79ee7e3d';
+update members set deleted_at = now()
+where member_id not in ('9cebe7ab-d43c-4a28-9caf-571a79ee7e3h');
 
-insert into tokens2(id)
+insert into members(member_id)
 values
     ('9cebe7ab-d43c-4a28-9caf-571a79ee7e3h');
 
